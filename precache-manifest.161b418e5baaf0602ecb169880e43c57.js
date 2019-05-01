24ad03567f4cc2326e08b5e21d1739935be71ee5
Viardiny Mathieu <viardiny89@yahoo.com>
2019-05-01 08:55:32 -0700

    Updates

---
+++ precache-manifest.161b418e5baaf0602ecb169880e43c57.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/hama-draft/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8b758e63fdb8bdba74c2",
+    "url": "/hama-draft/static/css/main.dde89605.chunk.css"
+  },
+  {
+    "revision": "ef85c2a9e6ad5dcffc72",
+    "url": "/hama-draft/static/js/runtime~main.09ff7cea.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/hama-draft/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "ac65cc7de86b8838e895",
+    "url": "/hama-draft/static/js/2.d034ee52.chunk.js"
+  },
+  {
+    "revision": "f7329289aee11b561046d604424910ca",
+    "url": "/hama-draft/static/media/Founder.f7329289.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/hama-draft/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/hama-draft/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/hama-draft/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/hama-draft/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/hama-draft/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "8b758e63fdb8bdba74c2",
+    "url": "/hama-draft/static/js/main.ba14d07a.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/hama-draft/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/hama-draft/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/hama-draft/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/hama-draft/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/hama-draft/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/hama-draft/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/hama-draft/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/hama-draft/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/hama-draft/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ac65cc7de86b8838e895",
+    "url": "/hama-draft/static/css/2.646a67fc.chunk.css"
+  },
+  {
+    "revision": "4278b7d585f6241c73a6905b8c3d12b6",
+    "url": "/hama-draft/index.html"
+  }
+];
